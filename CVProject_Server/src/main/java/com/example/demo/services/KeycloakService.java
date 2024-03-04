bb434dd282099ff9a84721f28cd7937fd292193e
Ho Thanh Kien <143081364+KuaDtrai@users.noreply.github.com>
2024-03-04 10:43:46 +0700

    10h45_03/04/24

--- CVProject_Server/src/main/java/com/example/demo/services/KeycloakService.java
+++ CVProject_Server/src/main/java/com/example/demo/services/KeycloakService.java
@@ -4,54 +4,59 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-@AllArgsConstructor
 @Service
 public class KeycloakService {
 
+    private String url;
+    private String clientSecret;
+    private String clientId;
+    private RestTemplate restTemplate;
+
+    @Value("${keycloak.base-url}")
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Value("${keycloak.client-secret}")
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    @Value("${keycloak.client-id}")
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
     @Autowired
-    private final RestTemplate restTemplate;
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public Boolean introspectToken(String token) throws Exception {
-
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-
-
-        //ENV!!!!!!!!!!!!
-        body.add("client_id", "test");
-        body.add("client_secret", "6AxzOF92ephN5G7P15QQGG4hCG8bfPNI");
+        body.add("client_id", clientId);
+        body.add("client_secret", clientSecret);
         body.add("token", token);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-//        headers.add("client_id", clientId);
-//        headers.add("client_secret", clientSecret);
-//        headers.add("token", token);
-
-        //ENV!!!!!!!!!!!!
-        String url = "http://localhost:8080/realms/v1/protocol/openid-connect/token/introspect";
-
         HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(body, headers);
 
         ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
 
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(response.getBody());
+        boolean active = jsonNode.get("active").asBoolean();
 
-            // Parse JSON string to JsonNode
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode jsonNode = mapper.readTree(response.getBody());
-
-            // Get the value of "active"
-            boolean active = jsonNode.get("active").asBoolean();
-
-            return active;
-
+        return active;
     }
 }
