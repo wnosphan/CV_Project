10db23a203022186cfcff071073c05fc89967c77
wnosphan <158177389+wnosphan@users.noreply.github.com>
2024-03-21 13:52:17 +0700

    Add files via upload

---
+++ Mail/src/main/java/com/example/demo/repositories/CvRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.Cv;
+import lombok.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface CvRepository extends JpaRepository<Cv, Long> {
+    Page<Cv> findAll(Pageable pageable);
+    Integer countByStatusContaining (String status);
+
+
+}
---
+++ Mail/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u.email FROM User u")
+    List<String> getAllEmails();
+}
