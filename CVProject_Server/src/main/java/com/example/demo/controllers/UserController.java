48eb52a7248e85a14a73936ea8bdaec4d9c17d26
wnosphan <sson151201@gmail.com>
2024-04-02 09:59:46 +0700

    update save user

--- CVProject_Server/src/main/java/com/example/demo/controllers/UserController.java
+++ CVProject_Server/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import com.example.demo.dtos.UserDTO;
 import com.example.demo.models.User;
 import com.example.demo.services.UserService;
 import lombok.RequiredArgsConstructor;
@@ -16,8 +17,8 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
      @PostMapping("/save-user")
-    public void saveUser( @RequestHeader String username, @RequestHeader String email){
-        userService.saveUser(username, email);
+    public void saveUser(@RequestBody UserDTO userDTO){
+        userService.saveUser(userDTO);
     }
 
 }
