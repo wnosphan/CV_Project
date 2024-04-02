27966b833c9e88fe1fbb8c3d283d2905ffb8786d
Ho Thanh Kien <143081364+KuaDtrai@users.noreply.github.com>
2024-04-02 10:19:36 +0700

    update print log

--- CVProject_Server/src/main/java/com/example/demo/controllers/UserController.java
+++ CVProject_Server/src/main/java/com/example/demo/controllers/UserController.java
@@ -3,8 +3,10 @@ package com.example.demo.controllers;
 import com.example.demo.models.User;
 import com.example.demo.services.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("${api.prefix}/cv")
 @CrossOrigin(origins = "${fe.base-url}",
@@ -15,9 +17,17 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+<<<<<<< Updated upstream
      @PostMapping("/save-user")
     public void saveUser( @RequestHeader String username, @RequestHeader String email){
         userService.saveUser(username, email);
+=======
+
+    @PostMapping("/save-user")
+    public void saveUser(@RequestBody UserDTO userDTO) {
+        log.info("Request data: " + userDTO);
+        userService.saveUser(userDTO);
+>>>>>>> Stashed changes
     }
 
 }
--- CVProject_Server/src/main/java/com/example/demo/services/UserService.java
+++ CVProject_Server/src/main/java/com/example/demo/services/UserService.java
@@ -2,8 +2,9 @@ package com.example.demo.services;
 
 import com.example.demo.models.User;
 import com.example.demo.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
+@Slf4j
 @Service
 public class UserService {
     private UserRepository userRepository;
@@ -13,8 +14,18 @@ public class UserService {
     }
     public void saveUser(String username, String email){
         User user = new User();
+<<<<<<< Updated upstream
         user.setUserName(username);
         user.setEmail(email);
+=======
+        user.setUserName(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+        if(userRepository.findByUserName(user.getUserName()) != null){
+            log.warn("User already exists");
+            throw new RuntimeException("User already exists");
+        }
+        log.info("User: "+user+" added successfully!");
+>>>>>>> Stashed changes
         userRepository.save(user);
     }
 }
