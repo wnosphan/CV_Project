bec27ce5117d73dc0e3debddd234c5588507d71e
wnosphan <sson151201@gmail.com>
2024-04-10 11:12:46 +0700

    update search

--- CVProject_Server/src/main/java/com/example/demo/services/ICvService.java
+++ CVProject_Server/src/main/java/com/example/demo/services/ICvService.java
@@ -3,6 +3,7 @@ package com.example.demo.services;
 import com.example.demo.dtos.CvDTO;
 import com.example.demo.dtos.CvStatusDTO;
 import com.example.demo.dtos.ListCvIdDTO;
+import com.example.demo.dtos.SearchDTO;
 import com.example.demo.models.Cv;
 import com.example.demo.responses.CvResponse;
 import org.springframework.data.domain.Page;
@@ -15,7 +16,21 @@ public interface ICvService {
 //    public Page<CvResponse> getAllCv(int page, int size);
 //    public Page<CvResponse> getListCv(int page, int size, String username) throws Exception;
 
-    Page<CvResponse> searchCv(int page, int size, String username, String sortby, String sorttype, String fullname, List<String> skill, List<String> status, LocalDate dateOfBirth, List<String> university, String trainingSystem, String gpa, List<String> applyPosition) throws Exception;
+//    Page<CvResponse> searchCv(int page, int size, String username, String sortby, String sorttype, String fullname, List<String> skill, List<String> status, LocalDate dateOfBirth, List<String> university, String trainingSystem, String gpa, List<String> applyPosition) throws Exception;
+
+    //    @Override
+//    public Page<CvResponse> getListCv(int page, int size, String userName) throws Exception {
+//        User existingUser = userRepository.findByUserName(userName);
+//        if(existingUser != null){
+//            Pageable pageable = PageRequest.of(page, size, Sort.by("id").ascending());
+//            Page<Cv> cvPage = cvRepository.searchCv(pageable, existingUser.getUserName());
+//            log.info("Lấy thành công danh sách Cv");
+//            return cvPage.map(CvResponse::fromCv);
+//        }
+//        log.error("User "+userName+" not found!!!");
+//        return null;
+//    }
+    Page<CvResponse> searchCv(int page, int size, String username, SearchDTO SeacrhDTO) throws Exception;
 
     public Cv getCvById(Long id) throws Exception;
     public Cv createCv(String username,CvDTO cvDTO) throws Exception;
